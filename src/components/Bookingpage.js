f6fdcfdd762c8c05c094f2c167ce3257a21bda06
Akash Agrawal <akashagrawal0358@gmail.com>
2023-10-05 13:20:29 +0530

    Remove useState and use templateliteral to add custom css with bootstrap

--- src/components/Bookingpage.js
+++ src/components/Bookingpage.js
@@ -4,20 +4,11 @@ import styles from "../styles/BookingPage.module.css"
 
 const Bookingpage = () => {
 
-    const BookingPagestyle = {
-        width: '60vw',
-        height: '55vh',
-        backgroundColor: '#60AFCC',
-        marginLeft: '20vw',
-        marginTop: '4vh',
-        borderRadius: '10px',
-        boxShadow: '10px 12px 9px #0F303B'
-    }
     return (
         <>
             <div className={styles.container}>
-                <div>
-                    <span className='text-center'><h1>Water Order Request</h1></span>
+                <div className={styles.heading}>
+                    <span className='text-center '><h1>Water Order Request</h1></span>
                 </div>
                 {/* <div classNameName='px-5 py-5 ' style={BookingPagestyle}>
                </div> */}
@@ -27,26 +18,26 @@ const Bookingpage = () => {
                     <form action='https://formspree.io/f/xyyqzngl' method='post'>
 
                         <div className={styles.formEntry} >
-                            <label for="name">Name<superscript>*</superscript> </label>
+                            <label htmlFor="name">Name<superscript>*</superscript> </label>
                             <input type="text"  className="form-control" name='Name' id="name" placeholder="Enter Your Name" />
                         </div>
                         <div className={styles.formEntry}>
-                            <label for="number">Mobile No.<superscript>*</superscript></label>
+                            <label htmlFor="number">Mobile No.<superscript>*</superscript></label>
                             <input type="text" className="form-control"  name='Mobile_No'id="number" placeholder="Enter Mobile Number" />
 
                         </div>
                         <div className={styles.formEntry}>
-                            <label for="number">Quantity<superscript>*</superscript></label>
+                            <label htmlFor="number">Quantity<superscript>*</superscript></label>
                             <input type="number" className="form-control" name='Quantiy' id="number" placeholder="Enter Mobile Number" min="1" />
 
                         </div>
                         <div className={styles.formEntry}>
-                            <label for="email">Email address(Optional)</label>
+                            <label htmlFor="email">Email address(Optional)</label>
                             <input type="email" className="form-control"  name='Email'id="email" placeholder="Enter email" />
 
-                        </div>
+                        </div> 
                         <div className={styles.formEntry}>
-                            <label for="passcode">Passcode</label>
+                            <label htmlFor="passcode">Passcode</label>
                             <input type="passcode" className="form-control" name='Passcode' id="passcode" placeholder="Enter passcode" />
 
                         </div>
--- src/components/home.js
+++ src/components/home.js
@@ -1,18 +1,13 @@
-import React, { useState } from 'react'
+import React  from 'react'
 import Nav from './Nav'
 import waterImg from '../Images/waterImg.jpg'
-import styles from "../waterImg.module.css";
-import { Link } from 'react-router-dom';
+//import styles from "../waterImg.module.css";
 import '../App.css'
+import { Link } from 'react-router-dom';
+import styles from '../styles/Home.module.css'
 
 
 const Home = () => {
-  const [btncolor, setBtncolor] = useState('#0d6efd');
-  const btnstyle = {
-    color: "white",
-    backgroundColor: btncolor
-    //`${btncolor}`
-  }
 
   return (
     <>
@@ -29,10 +24,8 @@ const Home = () => {
             <span style={{ color: 'red', fontSize: "1.3em", fontWeight: "bold" }}>Water..!!</span>
           </p>
           <Link to='/bookingpage' >
-            <button className="btn btn-outline-success fs-2 homeButton "
-              style={btnstyle} type="submit"
-              onMouseEnter={() => setBtncolor('red')}
-              onMouseLeave={() => setBtncolor('#0d6efd')}
+            <button className = { `${styles.button}  btn btn-primary btn-lg` }
+              
             >
               Click here
             </button>
